f3448fdcf890004d332cf8d7dc4cc521e8d6f0a3
joshckidd <josh@larameekidd.com>
2026-02-07 15:33:58 -0500

    created schema for rolls

---
+++ sql/schema/002_rolls.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+CREATE TABLE aggregate_rolls (
+    id UUID NOT NULL PRIMARY KEY
+    ,created_at TIMESTAMP NOT NULL
+    ,updated_at TIMESTAMP NOT NULL
+    ,string TEXT NOT NULL
+    ,result INT NOT NULL
+);
+
+CREATE TABLE rolls (
+    id UUID NOT NULL PRIMARY KEY
+    ,created_at TIMESTAMP NOT NULL
+    ,updated_at TIMESTAMP NOT NULL
+    ,string TEXT NOT NULL
+    ,result INT NOT NULL
+    ,individual_rolls TEXT NOT NULL
+    ,aggregate_roll_id UUID NOT NULL REFERENCES aggregate_rolls ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE rolls;
+DROP TABLE aggregate_rolls;
