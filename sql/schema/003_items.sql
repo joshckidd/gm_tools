e21e0d60b18800bde9c8d1ee4122e737774a73b3
joshckidd <josh@larameekidd.com>
2026-02-23 13:01:16 -0500

    created schema for items

---
+++ sql/schema/003_items.sql
@@ -0,0 +1,44 @@
+-- +goose Up
+CREATE TABLE types (
+    id UUID NOT NULL PRIMARY KEY
+    ,created_at TIMESTAMP NOT NULL
+    ,updated_at TIMESTAMP NOT NULL
+    ,type_name TEXT NOT NULL
+    ,username TEXT NOT NULL REFERENCES users ON DELETE CASCADE
+);
+
+CREATE TABLE items (
+    id UUID NOT NULL PRIMARY KEY
+    ,created_at TIMESTAMP NOT NULL
+    ,updated_at TIMESTAMP NOT NULL
+    ,item_name TEXT NOT NULL
+    ,item_description TEXT NOT NULL
+    ,type_id UUID NOT NULL REFERENCES types ON DELETE CASCADE
+    ,username TEXT NOT NULL REFERENCES users ON DELETE CASCADE
+);
+
+CREATE TABLE custom_fields (
+    id UUID NOT NULL PRIMARY KEY
+    ,created_at TIMESTAMP NOT NULL
+    ,updated_at TIMESTAMP NOT NULL
+    ,custom_field_name TEXT NOT NULL
+    ,custom_field_type TEXT NOT NULL
+    ,type_id UUID NOT NULL REFERENCES types ON DELETE CASCADE
+    ,username TEXT NOT NULL REFERENCES users ON DELETE CASCADE
+);
+
+CREATE TABLE custom_field_values (
+    id UUID NOT NULL PRIMARY KEY
+    ,created_at TIMESTAMP NOT NULL
+    ,updated_at TIMESTAMP NOT NULL
+    ,custom_field_value TEXT NOT NULL
+    ,type_id UUID NOT NULL REFERENCES types ON DELETE CASCADE
+    ,custom_field_id UUID NOT NULL REFERENCES custom_fields ON DELETE CASCADE
+    ,username TEXT NOT NULL REFERENCES users ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE custom_field_values;
+DROP TABLE custom_fields;
+DROP TABLE items;
+DROP TABLE types;
